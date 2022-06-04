bdeaa406bfd6af67b010ced59fb5fd1f029230dd
Bibo Hao <haobibo@users.noreply.github.com>
2022-06-05 03:03:35 +0800

    fix API handler multipart process

--- src/aloha/service/v1/api_handler.py
+++ src/aloha/service/v1/api_handler.py
@@ -30,17 +30,21 @@ class APIHandler(web.RequestHandler):
         pass
 
     async def post(self):
-        try:
-            req = self.request.body
-            body = json.loads(req.decode('utf-8'))
-        except json.decoder.JSONDecodeError:  # invalid request body, cannot be parsed as JSON
-            return self.finish(_RESP_BAD_REQUEST)
+        content_type: str = self.request.headers.get('Content-Type', 'application/json; charset=utf-8')
+        if content_type.startswith('multipart/form-data'):  # only parse files when 'Content-Type' starts with 'multipart/form-data'
+            body_arguments = self.request.body_arguments
+        else:
+            try:
+                body = self.request.body.decode('utf-8')
+                body_arguments = json.loads(body)
+            except (UnicodeDecodeError, json.decoder.JSONDecodeError):  # invalid request body, cannot be parsed as JSON
+                return self.finish(_RESP_BAD_REQUEST)
 
         try:
-            salt_uuid = body.pop('salt_uuid')
-            app_id = body.pop('app_id')
-            sign = body.pop('sign')
-            data = body.pop('data')
+            salt_uuid = body_arguments.pop('salt_uuid')
+            app_id = body_arguments.pop('app_id')
+            sign = body_arguments.pop('sign')
+            data = body_arguments.pop('data')
         except KeyError:  # cannot find default key from parsed body
             return self.finish(_RESP_MISSING_ARGS)
 
@@ -56,7 +60,7 @@ class APIHandler(web.RequestHandler):
         except Exception as e:
             if LOG.level == logging.DEBUG:
                 self.LOG.error(e, exc_info=True)
-            return self.finish({'code': 5201, 'message': [str(e)]})
+            return self.finish({'code': 5201, 'message': [repr(e)]})
 
         resp = json.dumps(resp, ensure_ascii=False, default=str, separators=(',', ':'))
         return self.finish(resp)
